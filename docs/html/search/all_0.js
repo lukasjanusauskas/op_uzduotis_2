977ba4b6e8d2800b1d52e8cc1907dcb5ec749b6e
lukasjanusauskas <lukasjanusauskaslj@gmail.com>
2024-12-05 13:23:49 +0200

    Sugeneruota pradinė doxygen dokumentacija

---
+++ docs/html/search/all_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['objektinio_20programavimo_20_28ind_20studijos_29_20_202_20užduotis_0',['Objektinio programavimo (IND studijos)  2 užduotis',['../index.html',1,'']]]
+];
---
+++ docs/html/search/searchdata.js
@@ -0,0 +1,21 @@
+var indexSectionsWithContent =
+{
+  0: "ostz",
+  1: "stz",
+  2: "o"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "pages"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Classes",
+  2: "Pages"
+};
+
